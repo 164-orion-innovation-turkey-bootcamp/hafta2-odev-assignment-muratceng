27d729e7c6f59b68d9fb760e24f620e2fb906073
Murat Çalışkan <muratcengcaliskan@gmail.com>
2022-03-07 06:19:09 +0300

    comment line changed

--- homework.js
+++ homework.js
@@ -36,7 +36,6 @@ steps
   .set("6", "Ürünler servis tepsisine konuldu.")
   .set("7", "Müşteriye servis edildi.");
 
-
 // Verdiğimiz saniye boyunca bekleyip istediğimiz değişkeni konsola yazdıran fonksiyon.
 function newTodo(todo, sec) {
   return new Promise((resolve, reject) => {
@@ -46,7 +45,7 @@ function newTodo(todo, sec) {
   });
 }
 
-// müşterinin köfte seçmesi durumunda 
+// müşterinin köfte seçtiği durumda todolar üreten fonksiyon
 async function meatballPrep(steps, order) {
   await newTodo(steps.get("3.1.0"), 0);
   if (order.degree == "rare") {
@@ -59,14 +58,17 @@ async function meatballPrep(steps, order) {
   await newTodo(steps.get("3.2"), 2);
 }
 
+// müşterinin tavuk seçtiği durumda todolar üreten fonksiyon
 async function chickenPrep(steps, order) {
   await newTodo(steps.get("3.1"), 3);
   await newTodo(steps.get("3.2"), 2);
 }
 
-async function menuPrep(steps, order, stocks) {
+// müşterinin siparişinde 3,4,5 adımları aynı anda yürütülmesi gereken adımlardır bu amac icin promise dönen fonksiyondur.
+function menuPrep(steps, order, stocks) {
   return new Promise((resolve, reject) => {
     if (order.type == "meatball") {
+      // resolve içerisine todolar verildiği için bu todolar senkronize çalışır ve hepsinin işini bitirmesi beklenir.
       resolve(
         meatballPrep(steps, order),
         newTodo(steps.get("4"), 5),
@@ -84,10 +86,11 @@ async function menuPrep(steps, order, stocks) {
   });
 }
 
+// stok sayılarını azaltan fonksiyon
 function decreaseStocks(stocks, meatType) {
   Object.entries(stocks).map((element) => {
-    // element --> [key,value]
-    // console.log(element[0],element[1])
+    // stok listesini key,value olarak döner element[0] key değerini ifade eder.
+    // köfte veya tavuk olması durumuna göre azaltma işlemi gerçekleştirir.
     if (meatType == "meatball") {
       stocks[element[0]] -= 1;
       if (element[0] === "chicken") {
@@ -102,9 +105,12 @@ function decreaseStocks(stocks, meatType) {
   });
 }
 
+// prompts kullanarak müşterinin köfte tavuk seçimi yapmasını sağlar.
 async function chooseType(order) {
   const response = await prompts([
     {
+      // select tipi seçim yapma tipidir name cevabın atanacağı değişkendir. mesaj sorulacak soru, seçeneklerde title
+      // kullanıcıya görünen value bize ulaşan değerdir.
       type: "select",
       name: "meatType",
       message: "Tavuk burger mi Köfte Burger mi seçersiniz",
@@ -117,8 +123,11 @@ async function chooseType(order) {
   return response.meatType;
 }
 
+// prompts kullanarak müşterinin köftesinin pişme derecesini seçmesini sağlar sağlar.
 async function chooseMeatType() {
   const response = await prompts({
+    // select tipi seçim yapma tipidir name cevabın atanacağı değişkendir. mesaj sorulacak soru, seçeneklerde title
+    // kullanıcıya görünen value bize ulaşan değerdir.
     type: "select",
     name: "meatType",
     message: "Köftenizin pişme derecesi ne olsun?",
@@ -131,10 +140,11 @@ async function chooseMeatType() {
   return response.meatType;
 }
 
+// tüm sipariş döngüsünün yer aldığı fonksiyondur. stok bittiği durumda false döner.
 async function orderPreparation(steps, order, stocks) {
   await newTodo(steps.get("1"), 1);
   await newTodo(steps.get("2"), 2);
-  
+
   if (checkStocks(stocks)) {
     order.type = await chooseType();
     if (order.type == "meatball") {
@@ -144,38 +154,43 @@ async function orderPreparation(steps, order, stocks) {
     await newTodo(steps.get("6"), 1);
     await newTodo(steps.get("7"), 1);
   } else {
-    console.log('stok kalmamıştır.')
+    console.log("stok kalmamıştır.");
     return false;
   }
 }
 
+// order tanımı
 let order = {
   type: "",
   degree: "",
 };
 
+// dükkanın yönetimini sağlayan fonksiyondur. stok bitmedikçe ve müşteri sipariş istedikçe sonsuz döngüde çalışmaktadır.
 async function shop(steps, order, stocks) {
   while (true) {
-    if (await orderPreparation(steps, order, stocks)==false){
-        break;
-    };
+    // orderpreparation stok bittiği durumda false değeri dönmektedir bu durumda döngü kırılır.
+    if ((await orderPreparation(steps, order, stocks)) == false) {
+      break;
+    }
+    //müşterinin yeni bir sipariş isteme durumunu yöneten promptsdur. cevap response a atanır.
     const response = await prompts([
-        {
-          type: "select",
-          name: "isopen",
-          message: "Bir sipariş daha vermek ister misiniz?",
-          choices: [
-            { title: "evet", value: true },
-            { title: "hayır", value: false },
-          ],
-        },
-      ]);
-      if(!response.isopen){
-          console.log('iyi günler dileriz.')
-          break;
-      }
-    
+      {
+        type: "select",
+        name: "isopen",
+        message: "Bir sipariş daha vermek ister misiniz?",
+        choices: [
+          { title: "evet", value: true },
+          { title: "hayır", value: false },
+        ],
+      },
+    ]);
+    //response false olduğu durumda döngü kırılır.
+    if (!response.isopen) {
+      console.log("iyi günler dileriz.");
+      break;
+    }
   }
 }
 
+// program başlatılmıştır.
 shop(steps, order, stocks);
